f944638eb5ac40ce6e573fdf18406c3548005697
David S. Jackson <deepbsd@yahoo.com>
2017-03-03 01:52:03 -0500

    trying to find the right channels

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
-var YOUTUBE_BASE_URL1 = 'https://www.googleapis.com/youtube/v3/channels';
+var YOUTUBE_BASE_URL1 = 'https://www.googleapis.com/youtube/v3/channels.list';
+var API_KEY = 'AIzaSyA_MlA_Pv1yElF8PY9hy_Ak6Mmr6g2xltY';
 
 function getChannelsList(searchTerm, callback) {
   var settings = {
     url: YOUTUBE_BASE_URL,
     data: {
-      part: 'contentDetails',
+      part: 'snippet',
       maxResults: 15,
       order: 'rating',
-      regionCode: 'US',
+      //regionCode: 'US',
       relevanceLanguage: 'en',
       type: 'channel',
-      key: 'AIzaSyA_MlA_Pv1yElF8PY9hy_Ak6Mmr6g2xltY',
+      key: API_KEY,
       publishedAfter: '2016-01-01T00:00:00.00Z',
       q: searchTerm,
       r: 'json',
@@ -33,7 +34,7 @@ function getDataFromApi(searchTerm, callback) {
       regionCode: 'US',
       relevanceLanguage: 'en',
       type: 'video',
-      key: 'AIzaSyA_MlA_Pv1yElF8PY9hy_Ak6Mmr6g2xltY',
+      key: API_KEY,
       publishedAfter: '2016-01-01T00:00:00.00Z',
       q: searchTerm,
       r: 'json',
@@ -53,7 +54,7 @@ function displayYouTubeSearchData(data) {
   if (data.items) {
     data.items.forEach(function(item) {
       try {
-        resultElement += '<div class="video-box"><figure><a href="http://www.youtube.com/watch?v='+item.id.videoId+'"><img src="'+item.snippet.thumbnails.medium.url+'"/><figcaption class="caption" style="width:350px">'+item.snippet.description+'</figcaption></a></figure></div>';
+        resultElement += '<div class="video-box"><figure><a href="http://www.youtube.com/channel/'+item.id.channelId+'"><img src="'+item.snippet.thumbnails.medium.url+'"/><figcaption class="caption" style="width:350px">'+item.snippet.description+'</figcaption></a></figure></div>';
       } catch (e) {
         console.log('Error: '+e);
       }
@@ -71,8 +72,8 @@ function watchSubmit() {
     e.preventDefault();
     var query = $(this).find('.js-query').val();
     console.log('Query string: '+query);
-    getDataFromApi(query, displayYouTubeSearchData);
-    //getChannelsList(query, displayYouTubeSearchData);
+    //getDataFromApi(query, displayYouTubeSearchData);
+    getChannelsList(query, displayYouTubeSearchData);
   });
 }
 
