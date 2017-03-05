58e235f0a6a2945a795eb3dbcd594a0c8f28ecbf
David S. Jackson <deepbsd@yahoo.com>
2017-03-04 23:12:09 -0500

    still slogging along...

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search.list';
 var YOUTUBE_BASE_URL1 = 'https://www.googleapis.com/youtube/v3/channels.list';
+var YOUTUBE_BASE_URL2 = 'https://gdata.youtube.com/feeds/api/standardfeeds/mostPopular';
 var API_KEY = 'AIzaSyA_MlA_Pv1yElF8PY9hy_Ak6Mmr6g2xltY';
 
 function getChannelsList(searchTerm, callback) {
@@ -9,7 +10,7 @@ function getChannelsList(searchTerm, callback) {
       part: 'snippet',
       maxResults: 15,
       order: 'rating',
-      //regionCode: 'US',
+      regionCode: 'US',
       relevanceLanguage: 'en',
       type: 'channel',
       key: API_KEY,
@@ -24,13 +25,14 @@ function getChannelsList(searchTerm, callback) {
   $.ajax(settings);
 }
 
-function getDataFromApi(searchTerm, callback) {
+function getMostPopularVideos(searchTerm, callback) {
   var settings = {
-    url: YOUTUBE_BASE_URL,
+    url: YOUTUBE_BASE_URL2,
     data: {
       part: 'snippet',
+      chart: 'mostPopular',
       maxResults: 15,
-      order: 'rating',
+      //order: 'rating',
       regionCode: 'US',
       relevanceLanguage: 'en',
       type: 'video',
@@ -54,6 +56,8 @@ function displayYouTubeSearchData(data) {
   if (data.items) {
     data.items.forEach(function(item) {
       try {
+        // One is for channels, the other for videos
+        //resultElement += '<div class="video-box"><figure><a href="http://www.youtube.com/channel/'+item.id.channelId+'"><img src="'+item.snippet.thumbnails.medium.url+'"/><figcaption class="caption" style="width:350px">'+item.snippet.description+'</figcaption></a></figure></div>';
         resultElement += '<div class="video-box"><figure><a href="http://www.youtube.com/channel/'+item.id.channelId+'"><img src="'+item.snippet.thumbnails.medium.url+'"/><figcaption class="caption" style="width:350px">'+item.snippet.description+'</figcaption></a></figure></div>';
       } catch (e) {
         console.log('Error: '+e);
@@ -72,8 +76,8 @@ function watchSubmit() {
     e.preventDefault();
     var query = $(this).find('.js-query').val();
     console.log('Query string: '+query);
-    //getDataFromApi(query, displayYouTubeSearchData);
-    getChannelsList(query, displayYouTubeSearchData);
+    getMostPopularVideos(query, displayYouTubeSearchData);
+    //getChannelsList(query, displayYouTubeSearchData);
   });
 }
 
