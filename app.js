d3ce97092b364eb61ee3565d6490a8a2c39f33e7
David S. Jackson <deepbsd@yahoo.com>
2017-03-08 09:13:50 -0500

    Big changes to app.js for adding promises and not using callbacks like before

--- app.js
+++ app.js
@@ -1,8 +1,101 @@
+/*
 var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 var YOUTUBE_BASE_URL1 = 'https://www.googleapis.com/youtube/v3/channels';
 var YOUTUBE_BASE_URL2 = 'https://gdata.youtube.com/feeds/api/standardfeeds/mostPopular';
+*/
+
+var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3';
 var API_KEY = 'AIzaSyA_MlA_Pv1yElF8PY9hy_Ak6Mmr6g2xltY';
 
+//Accept a YouTube endpoint and some URL parameters (a key-value object)
+//and return a pre-formatted string to be used as a target for an HTTP request.
+function youTubeRequest(endpoint, parameters) {
+  parameters = Object.assign({}, {key: API_KEY}, parameters)
+  parameters = Object.keys(parameters)
+    .map(function (key) {
+      return key + '=' + parameters[key]
+    })
+    .join('&')
+
+  var url = '@baseurl/@endpoint?@parameters'
+    .replace('@baseurl', YOUTUBE_BASE_URL)
+    .replace('@endpoint', endpoint)
+    .replace('@parameters', parameters)
+  console.log({url: url})
+
+  return jQuery.get(url, {dataType: 'json'})
+
+}  // End of youTubeRequest()
+
+
+// Once we have the list of content providers, we use this function to
+// return their top videos
+function getUserVideos(username) {
+  return youTubeRequest('channels', {      // should this be 'channels' or 'videos'?
+    part: 'snippet',
+    forUsername: username
+  })
+    .then(function(payload) {
+      if (payload.items.length <= 0) {
+        throw new Error('YouTube Data API does not return any videos for the given username')
+      }
+
+      return jQuery.get(youTubeRequest('search', {
+        channelId: payload.items[0].id,
+        type: 'video',           
+        part: 'snippet',
+        maxResults: 50
+      }), {
+        dataType: 'json'
+      })
+    })
+      .then(function(payload) {
+        if (payload.items.length <= 0) {
+          throw new Error('YouTube Data API did return any videos for the given channel')
+        }
+
+        var videoPromises = payload.items.map(function(video) {
+          return jQuery.get(youTubeRequest('videos', {
+            id: video.id.videoId,
+            part: 'contentDetails,statistics'
+          }), {
+            dataType: 'json'
+          })
+        })
+
+        return Promise.all(videoPromises)
+      })
+        .then(function(videos) {
+          console.log("Wow! Too much work to get those videos:", videos)
+        })
+        .catch(function(e) {
+          console.log('Something went wrong:', e)
+        })
+}  // End of getUserVideos()
+
+
+// This function finds the best channels for a user-specified query string
+function queryBestRatedChannelsFor(q) {
+  youTubeRequest('search', {
+    type: 'channel',
+    part: 'snippet',
+    q: q,
+    order: 'rating',
+    maxResults: 50
+  })
+    .then(function(payload) {
+      console.log('queryBestRatedChannelsFor', payload)
+      displayYouTubeSearchData(payload)
+    })
+    .catch(function(e) {
+      console.log('Error: ', e)
+    })
+}  // End of queryBestRatedChannelsFor()
+
+
+
+/*
+// Not sure if I want to use this function again...
 function getChannelsList(searchTerm, callback) {
   var settings = {
     url: YOUTUBE_BASE_URL1,
@@ -49,9 +142,9 @@ function getMostPopularVideos(searchTerm, callback) {
   };
   $.ajax(settings);
 }
+*/
 
-
-function displayYouTubeSearchData(data) { debugger;
+function displayYouTubeSearchData(data) {
   var resultElement = '<h2>Results for '+data.items[0].snippet.channelTitle+'</h2>';
   console.log('Here\'s the object: '+JSON.stringify(data));
 
@@ -79,7 +172,8 @@ function watchSubmit() {
     var query = $(this).find('.js-query').val();
     console.log('Query string: '+query);
     //getMostPopularVideos(query, displayYouTubeSearchData);
-    getChannelsList(query, displayYouTubeSearchData);
+    //getChannelsList(query, displayYouTubeSearchData);
+    queryBestRatedChannelsFor(query)
   });
 }
 
